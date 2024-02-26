6567596c615583204c00055fea6b8b1564d8eed7
BrianWanamaker <bcwanamaker@quinnipiac.edu>
2024-02-26 01:10:55 -0500

    updated so total word counts works.. still off by a few words

--- Client.java
+++ Client.java
@@ -7,29 +7,29 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) {
         String serverIP = "127.0.0.1";
-        int serverPort = 12345; 
+        int serverPort = 12345;
 
         try (Socket socket = new Socket(serverIP, serverPort)) {
             System.out.println("Connected to server.");
 
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            // try {
+            // Thread.sleep(10000);
+            // } catch (InterruptedException e) {
+            // e.printStackTrace();
+            // }
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String filePath = reader.readLine();
-            
+
             int wordCount = WordCount.wordCount(filePath);
             String wordCount2 = String.valueOf(wordCount);
             System.out.println("Number of words in the file: " + wordCount);
 
             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-            writer.println(wordCount2); 
+            writer.println(wordCount2);
             writer.flush();
-            writer.close();  
-            socket.close();          
+            writer.close();
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- Server.java
+++ Server.java
@@ -14,50 +14,53 @@ public class Server {
             System.out.println("Server started. Waiting for clients to connect...");
 
             byte[] originalFileContent = Files.readAllBytes(Paths.get(originalFileName));
-
             int segmentSize = originalFileContent.length / numberOfClients;
 
             for (int i = 0; i < numberOfClients; i++) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Client connected: " + (i + 1) + "/" + numberOfClients);
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    System.out.println("Client connected: " + (i + 1) + "/" + numberOfClients);
 
-                int startIndex = i * segmentSize;
-                int endIndex;
-                if (i == numberOfClients - 1) {
-                    endIndex = originalFileContent.length;
-                } else {
-                    endIndex = (i + 1) * segmentSize;
+                    int startIndex = i * segmentSize;
+                    int endIndex;
+                    if (i == numberOfClients - 1) {
+                        endIndex = originalFileContent.length;
+                    } else {
+                        endIndex = (i + 1) * segmentSize;
+                    }
+                    byte[] segment = extractSegment(originalFileContent, startIndex, endIndex);
+
+                    String segmentFileName = "segment_" + i + ".txt";
+                    saveToFile(segmentFileName, segment);
+
+                    sendFileNameToClient(clientSocket, segmentFileName);
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    String wordCountStr = in.readLine();
+                    if (wordCountStr != null) {
+                        int wordCount = Integer.parseInt(wordCountStr);
+                        System.out.println("Client sent word count: " + wordCount);
+                        totalWordCount += wordCount;
+                    } else {
+                        System.out.println("No word count received, client may have closed the connection.");
+                    }
+
+                    in.close();
+                    clientSocket.close();
+                    System.out.println("Client disconnected.");
+                } catch (IOException e) {
+                    System.out.println("An error occurred with a client connection.");
+                    e.printStackTrace();
                 }
-                byte[] segment = extractSegment(originalFileContent, startIndex, endIndex);
-
-                // String segmentContent = new String(segment);
-                // // Print the content
-                // System.out.println("Segment content: " + segmentContent);
-
-                String segmentFileName = "segment_" + i + ".txt";
-                saveToFile(segmentFileName, segment);
-
-                sendFileNameToClient(clientSocket, segmentFileName);
-
-                // BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-                // String wordCountStr = in.readLine(); 
-                // int wordCount = Integer.parseInt(wordCountStr);
-                // System.out.println("Client sent word count: " + wordCount);
-
-                // totalWordCount += wordCount;
-
-                // in.close();
-                clientSocket.close();
-                System.out.println("Client disconnected.");
             }
-            System.out.println(totalWordCount);
+
+            System.out.println("Total word count from all clients: " + totalWordCount);
         } catch (IOException e) {
+            System.out.println("An error occurred starting the server.");
             e.printStackTrace();
         }
     }
 
-    // this method will extract the specific content for each client. it will return a byte array containing the extracted segment of the file content.
     private static byte[] extractSegment(byte[] fileContent, int startIndex, int endIndex) {
         int segmentSize = endIndex - startIndex;
         byte[] segment = new byte[segmentSize];
@@ -65,17 +68,14 @@ public class Server {
         return segment;
     }
 
-    // this method will make a file with the content in it.
     private static void saveToFile(String fileName, byte[] content) throws IOException {
         try (FileOutputStream fos = new FileOutputStream(fileName)) {
             fos.write(content);
         }
     }
 
-    // this method will send the file to client
     private static void sendFileNameToClient(Socket clientSocket, String fileName) throws IOException {
-        try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
-            out.println(fileName);
-        }
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        out.println(fileName);
     }
 }
